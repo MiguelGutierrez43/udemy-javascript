c3475056df2de5f69ae1d87b78d4b1982ee6d76a
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-01-21 15:55:30 -0300

    clase 3 completa

--- variables.js
+++ variables.js
@@ -1,5 +1,22 @@
 // var: variables locales
 var saludo = 'Hola Miguel'
+if (saludo) {
+
+    var saludo = 'Hola'
+    console.log(saludo)
+}
 console.log(saludo)
+
 // let: variables limitadas al alcance del bloque donde las definimos
+let nombre = 'Miguel'
+if (nombre) {
+
+    let nombre = 'Claudia'
+    console.log(nombre)
+    
+}
+console.log(nombre)
+
 // const: son variables inmutables
+const PI = 3.14
+console.log(PI)
