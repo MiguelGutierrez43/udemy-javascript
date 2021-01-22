c68cb95c1d79a86c0e5778f071116fb664fe71cf
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-01-22 10:43:44 -0300

    Tipos de variable Completado

--- tiposV.js
+++ tiposV.js
@@ -1,2 +1,19 @@
 var nombre = "Miguel"
-console.log(nombre)
+console.log(nombre) // string
+var miNumero = 50
+console.log(miNumero) // number
+//const PI = 3.14
+//console.log(PI) //Decimal
+var verdadero = true //Boolean true
+var falso = false  //Boolean false
+console.log(verdadero)
+console.log(falso)
+var nulo = null //null
+console.log(nulo)
+var indefinido = undefined
+console.log(indefinido)
+
+console.log(typeof miNumero)
+
+const PI = Math.PI
+console.log(PI)
