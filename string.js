b6906f54fdbda75b8ccbfee405ab2ca078c4e345
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-01-22 15:20:17 -0300

    Variables string y sus metodos

---
+++ string.js
@@ -0,0 +1,11 @@
+var nombre = 'Miguel'
+var edad = 46
+
+console.log('Mi nombre es: ' + nombre + ' y tengo ' + edad + 'años')
+console.log('Mi nombre es ${nombre} y tengo ${edad} años')
+
+var nombreMayus = nombre.toUpperCase()
+
+console.log(nombreMayus)
+console.log(nombre.toLowerCase())
+console.log(nombre.length)
