e7e4b034c4e08520ce17f7d3e737fd8433fc063b
MiguelGutierrez43 <mgutierrez@siancolors.cl>
2021-01-26 11:44:42 -0300

    Termino de Clase sobre operadores

---
+++ operadores.js
@@ -0,0 +1,13 @@
+// +, -, *, /, %, **, ++, -- Son los operadores aritmeticos
+// =; +=, -=, *=, /=, %=, **=. operadores de asignación
+// ==, !=, ===, !==, >, >=, <, <=. Operadores de comparación
+// && (AND), || (OR), !. Operadores logicos
+
+var v = true
+var f = false
+
+// Operador ternario: condicion ? true : false
+
+var edad = 16
+var resp = edad >= 18 ? console.log("Mayor de Edad") : console.log("Menor de edad")
+console.log(resp)
