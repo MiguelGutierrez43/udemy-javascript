caf215aae45698ac276b0d702c9e4eb62d6acd6b
Miguel <mgutierrez@siancolors.cl>
2021-02-06 16:04:31 -0300

    Clase terminada funcion de flecha

--- Funciones/arrowFunctions.js
+++ Funciones/arrowFunctions.js
@@ -1,9 +1,21 @@
 var acceso = true
 
-var accesoU = function (a) {
+/*var accesoU = function (a) {
     return a
+}*/
+
+//var accesoU = a => a // Retorna un solo valor
+
+//var accesoU = () => false // No pasamos parametros a nuestra funcion y retornamos un valor
+
+//var accesoU = (a,n) => console.log(`Usuario ${n} Acceso ${a}`) // Cuando pasamos mas de un parametro
+//accesoU(acceso, "Miguel")
+
+var accesoU = (a,nombre) => {
+    console.log(`Usuario ${nombre} en ejecución`)
+    return true
 }
 
-accesoU(acceso) == true
+accesoU(acceso, "Miguel") == true
 ? console.log("Acceso Permitido")
 : console.log("Acesoo Denegado")
