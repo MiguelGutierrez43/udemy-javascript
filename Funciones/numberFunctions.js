02df7c885cedda9eedc3cc411bf9bcf6a713b53a
Miguel <mgutierrez@siancolors.cl>
2021-02-06 16:20:46 -0300

    Termino de la clase number functions

--- Funciones/numberFunctions.js
+++ Funciones/numberFunctions.js
@@ -1,5 +1,17 @@
-var numero = 50.00093
+/*var numero = 50.00093
 
 var res = Number.isInteger(numero)
+
+console.log(res)*/
+
+/*var numero = 50.234
+
+var res = Number.parseInt(numero)
+
+console.log(res)*/
+
+var numero = 50.63747
+
+var res = Number.parseFloat(numero).toFixed(3)
 
 console.log(res)
